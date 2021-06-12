d9d4114409c32a5437b544edb736d9e9085e4784
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-06-13 01:04:57 +0700

    update checkout page

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -5,9 +5,11 @@
 @endpush
 
 @section('content')
-<div class="flex flex-col">
-    <h1 class="text-white text-3xl font-bold">
-        YAY
+<div class="flex flex-col items-center">
+    <img class="mt-n12" width="500px" height="500px" src="{{asset('assets/img/happy.svg')}}" alt="">
+    <h1 class="mt-24 text-center text-white text-2xl font-bold">
+        YAY! Selamat, {{$user->name}} Kamu telah terdaftar.<br> Tinggal tunggu sesuai jadwal ya!
     </h1>
+    <a class="text-white mt-4 text-lg underline" href="{{route('dashboard')}}">Kembali ke Home</a>
 </div>
 @endsection
